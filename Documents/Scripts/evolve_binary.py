Wrote new module function_tests.py for debugging. Edited orbitalElements, needs more work, still getting nans or wrong values.

Documents/Scripts/evolve_binary.py
@@ -29,9 +29,9 @@ def evolveBinary(X, m1, m2, t):
         X[3] = np.array([- n*a/(math.sqrt(1.0-e**2.0)) * math.sin(f_new), n*a/(math.sqrt(1.0-e**2.0)) * (e + math.cos(f_new)), 0.0])
         '''
         #Rotate into reference frame
-        R = [[np.cos(Omega)*np.cos(omega) - np.sin(Omega)*np.cos(I)*np.sin(omega), -np.cos(Omega)*np.sin(omega)-np.sin(Omega)*np.cos(I)*np.cos(omega), np.sin(I)*np.sin(Omega)],
+        R = np.array([[np.cos(Omega)*np.cos(omega) - np.sin(Omega)*np.cos(I)*np.sin(omega), -np.cos(Omega)*np.sin(omega)-np.sin(Omega)*np.cos(I)*np.cos(omega), np.sin(I)*np.sin(Omega)],
              [np.sin(Omega)*np.cos(omega) + np.cos(Omega)*np.cos(I)*np.sin(omega), -np.sin(Omega)*np.sin(omega)+np.cos(Omega)*np.cos(I)*np.cos(omega),-np.sin(I)*np.cos(Omega)],
-             [np.sin(I)*np.sin(omega), np.sin(I)*np.cos(omega), np.cos(I)]]
+             [np.sin(I)*np.sin(omega), np.sin(I)*np.cos(omega), np.cos(I)]])
         X[1] = np.transpose(np.dot(R, np.transpose(X[1])))
         X[3] = np.transpose(np.dot(R, np.transpose(X[3])))
         '''

Documents/Scripts/orbital_elements.py
@@ -18,7 +18,11 @@ def orbitalElements(X, m1, m2):
         V = np.linalg.norm(X[2]-X[3])
         R_dot_Rdot = np.dot((X[0]-X[1]),(X[2]-X[3]))
         h = np.cross((X[0]-X[1]),(X[2]-X[3]))
-        R_dot = np.sign(R_dot_Rdot)*np.sqrt(V**2.0 - np.dot(h,h)/(R**2.0))
+        if abs(V**2.0 - np.dot(h,h)/(R**2.0)) > 10.0**(-8.0):
+                R_dot = np.sign(R_dot_Rdot)*np.sqrt(V**2.0 - np.dot(h,h)/(R**2.0))
+        else:
+                R_dot = 0.0
+                        
         
         #Semi-major axis
         a = (2.0/R - V**2.0/(G*(m1+m2)))**(-1.0)
@@ -28,17 +32,17 @@ def orbitalElements(X, m1, m2):
         I = np.arccos(h[2]/np.linalg.norm(h))
         #True anomaly
         f = np.arcsin(a*(1.0-e**2.0)*R_dot/(np.linalg.norm(h)*e))
+        #
+        Omega = np.arctan(-h[0]/h[1])
+        print(h[1])
+        print(-h[0]/h[1])
+        print(Omega)
         if I != 0.0:
-                #
-                Omega = np.arcsin(np.sign(h[2])*h[0]/(np.linalg.norm(h)*np.sin(I)))
-                omega_plus_f = np.arcsin((X[0,2]-X[1,2])/(R*np.sin(I)))
-                #
+                omega_plus_f = np.arcsin((X[0,2]-X[1,2])/R*np.sin(I))
                 omega = omega_plus_f - f
         else:
-                #
-                Omega = 0
-                omega_plus_f = np.arccos((X[0,0]-X[1,0])/R)
-                #
-                omega = omega_plus_f - f
+                omega = 0.0
+                
+        print(omega)
                 
         return(a, e, f, I, Omega, omega)