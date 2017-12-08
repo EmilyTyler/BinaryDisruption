92bdfc57e695646eb3a613bef29eb1880a866a6c
EmilyTyler <emily.tyler@nottingham.ac.uk>
2017-12-08 17:38:34 +0000

    Plotted actual pdf over generated pdf in maxwellian_text.pyx, it doesn't match. Wrote function to draw b value and function to simulate multiple encounters using the Monte Carlo method in monte_carlo.pyx, as yet untested.

--- Documents/Scripts/maxwellian_test.py
+++ Documents/Scripts/maxwellian_test.py
@@ -6,11 +6,13 @@ os.system("python setup.py build_ext --inplace")
 
 from matplotlib import pyplot as plt
 from monte_carlo import draw_maxwellian
+from monte_carlo import maxwellianPdf
+from monte_carlo import maxwellianComparison
 
 v_rms = 10.0**5.0
-v_max = 10.0**6.0
-v_min = 10.0**(-3.0)
-N = 10**5
+v_max = 10.0**1.0 * v_rms
+v_min = 10.0**(-3.0) * v_rms
+N = 10**4
 
 max_dist = draw_maxwellian(v_rms, v_min, v_max, N)
 
@@ -24,9 +26,16 @@ p = np.zeros(N_v, dtype = int)
 for x in max_dist:
         i = int(np.floor(np.log(x/v_min)/dlogv))
         p[i] += 1
-        
-     
+
+N_acc = np.size(max_dist)
+
+#Normalise
+p = p /(np.max(p)) * 4.0*(2.0*np.pi)**(-0.5)/v_rms*np.exp(-1.0)
 plt.plot(v_bins/1000.0, p)
+pdf = np.array([maxwellianPdf(v, v_rms) for v in v_bins])
+plt.plot(v_bins/1000.0, pdf)
+comparison = [maxwellianComparison(v, v_rms, v_min, v_max) for v in v_bins]
+#plt.plot(v_bins/1000.0, comparison)
 plt.xlabel('v, km/s')
 plt.ylabel('pdf')
 plt.show()
--- Documents/Scripts/monte_carlo.pyx
+++ Documents/Scripts/monte_carlo.pyx
@@ -4,7 +4,11 @@
 import random
 import numpy as np
 cimport numpy as np
+from cpython cimport bool
 
+from encounters import calc_b_max
+from encounters import encounterRate
+from encounters import encounter
 
 def draw_maxwellian(double v_rms, double v_min, double v_max, int N):      
         #Accepted points
@@ -22,19 +26,49 @@ def draw_maxwellian(double v_rms, double v_min, double v_max, int N):
         return accepted
 
 
-def maxwellianPdf(x, v_rms):     
-        return 4.0*np.pi*x**2.0*np.exp(-x**2.0/(2.0*v_rms**2.0))*(2.0*np.pi*v_rms**2.0)**(-3.0/2.0)
+def maxwellianPdf(double x, double v_rms):     
+        return 4.0*np.pi*x**2.0*np.exp(-x**2.0/(2.0*v_rms**2.0))*((2.0*np.pi*v_rms**2.0))**(-3.0/2.0)
 
-def maxwellianComparison(x, v_rms, v_min, v_max):      
+def maxwellianComparison(double x, double v_rms, double v_min, double v_max):      
+        cdef double answer
         if v_min<x<v_max:
                 answer = 4.0*np.exp(-1.0)*(2.0*np.pi)**(-0.5)/v_rms
         else:
                 answer = 0.0
         return answer
 
-def maxwellianX_from_area(A, v_rms, v_min):      
+def maxwellianX_from_area(double A, double v_rms, v_min):      
         return (2.0*np.pi)**0.5*v_rms*np.exp(1.0)*A/4.0 + v_min
 
+def draw_b(double b_max, int N):
+        return b_max*np.sqrt(random.uniform(0.0, 1.0, size=N))
+
+def MCEncounters(double v_rms, double n_p, double T, double m1, double m2, double M_p, double a, double e):
+        #Minimum impact parameter
+        cdef double b_min = 10.0**(-2.0)*(np.pi*n_p*v_rms*(10.0**10.0*365.25*24.0*60.0*60.0))**(-0.5)
+        #Maximum impact parameter
+        cdef double b_max = calc_b_max(M_p, v_rms, a, m1, m2)
+        #Minimum velocity
+        cdef double v_min = 10.0**(-2.0)*v_rms
+        #Maximum velocity
+        cdef double v_max = 10.0**2.0*v_rms
+        #Total number of encounters in time T 
+        cdef int N = int(T*encounterRate(n_p, v_rms, b_min, b_max, v_min, v_max))
+        #Impact parameters of encounters
+        cdef np.ndarray b = draw_b(b_max, N)
+        #Relative velocities of encounters
+        cdef np.ndarray v = draw_maxwellian(v_rms, v_min, v_max, N)
+        #Implement encounters
+        cdef bool notBound = False
+        cdef double a_new, e_new
+        for i in range(N):
+                (notBound, a_new, e_new) = encounter(m1, m2, v[i], b[i], a, e, M_p)
+                if notBound:
+                        print('Binary broken!')
+                        return (a, e)
+                a = a_new
+                e = e_new
+        return (a, e)
 
 
 
