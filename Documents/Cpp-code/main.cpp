0ec28212042a3e9f58fcbdf03083535839e85273
EmilyTyler <emily.tyler@nottingham.ac.uk>
2018-12-10 21:03:51 +0000

    Got compilation working on windows

--- Documents/Cpp-code/main.cpp
+++ Documents/Cpp-code/main.cpp
@@ -5,14 +5,22 @@
 using namespace std;
 
 int main() {
+
+    //cout << "Hello Simon!" << endl;
+
+    
 	array<double, 3> arr;
 
 	ofstream myfile;
 	myfile.open("test_data.csv");
 	for (int i; i<1000; i++){
 		arr = randomDirection();
+        //cout << "wakka";
+        //cout << arr[0] << endl;
 		myfile << arr[0] << "," << arr[1] << "," << arr[2] << endl; 
 	}
+    myfile.close();
+    
 
 }
 
