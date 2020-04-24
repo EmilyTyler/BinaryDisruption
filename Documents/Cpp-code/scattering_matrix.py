5d7d2480296fea4c721c3ed8cf4672675030dfe5
EmilyTyler <emily.tyler@nottingham.ac.uk>
2020-04-24 15:43:44 +0100

    edit to run

---
+++ Documents/Cpp-code/scattering_matrix.py
@@ -0,0 +1,11 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rc('font', family='serif')
+
+#Set up scattering matrix
+r_min = 
+
+with open() as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
